0b89c88478b52f9b303141f4628e5bcc9f3817fd
Idel Fatkhullin <maximbravin@gmail.com>
2023-07-22 22:38:22 +0600

    sec

---
+++ js/modules/tabs.js
@@ -0,0 +1,40 @@
+export default function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, tabsActiveClass) {
+    // Tabs
+    
+	let tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsParentSelector);
+
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(tabsActiveClass);
+        });
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(tabsActiveClass);
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+    });
+}
